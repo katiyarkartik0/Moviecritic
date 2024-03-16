655626db8b5f895168b914215cda2aa8c299d65f
Newton School <newtonschool@Newtons-MacBook-Pro.local>
2024-03-17 00:41:54 +0530

    added suspense wrapper around modal

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Modal from "@/components/Modal";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +22,10 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         {children}
-        <Modal/>
+        <Suspense fallback={<>Loading...</>}>
+          {" "}
+          <Modal />
+        </Suspense>
       </body>
     </html>
   );
